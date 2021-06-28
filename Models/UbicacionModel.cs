1ed5f1119f821aa008b69f3c45bd9ea997f128b1
EsmeraldaGPuc <64551428+EsmeraldaGPuc@users.noreply.github.com>
2021-06-27 22:34:24 -0500

    Ubicacion con LocalStorage

---
+++ Models/UbicacionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class UbicacionModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string Zona { get; set; }
+
+        public string Estante { get; set; }
+    }
+}
