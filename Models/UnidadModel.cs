f1a21ff37db5eca4d6acb3e79ca38c9973d9ba54
Nidia123 <nidia.arroyo@puntosingular.com>
2021-07-05 13:47:02 -0500

    ajustes Unidad

--- Models/UnidadModel.cs
+++ Models/UnidadModel.cs
@@ -7,15 +7,31 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class UnidadModel
+    public class UnidadModel : IValidatableObject
     {
-        [Required]
-        public int Id { get; set; }
+        [RegularExpression(@"^UNI-[A-Z]{4}-[0-9]{2}",
+         ErrorMessage = "Caracteres no permitidos.UNI-XXXX-00")]
+        [Required(ErrorMessage = "La clave es requerida")]
+        public string Id { get; set; }
 
-        [Required]
-        public string? Nombre { get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "El simbolo es requerido")]
+        [StringLength(3, ErrorMessage = "Simbolo es muy largo.")]
         public string Simbolo { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El status es requerido")]
+        public string Estatus { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Id == Simbolo)
+            {
+                yield return new ValidationResult(
+                    $"El id {Id} ya existe.",
+                    new[] { nameof(Id) });
+            }
+        }
     }
 }
