9247036b9351d80dc5c2a51bc77b01ca20d25249
Elmer Juventino Yam Ramirez <elmer.yam@puntosingular.com>
2021-07-22 00:26:06 -0500

    Crear funciones de agregar y eliminar factura

---
+++ Models/DetalleFacturaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DetalleFacturaModel
+    {
+        public int Id { get; set; }
+        public string FacturaId { get; set; }
+        public ArticuloModel Articulo { get; set; }
+        public int CantidadArticulos { get; set; }
+        public double Subtotal => CantidadArticulos * Articulo.PrecioUnitario;
+        public double Iva => Subtotal * 0.16;
+        public double Total => Subtotal + Iva;
+    }
+}
