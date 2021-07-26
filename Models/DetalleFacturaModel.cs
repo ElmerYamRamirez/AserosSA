d6f1dacc6e4243ddd9e47054a1f0795de2f0d6cd
Elmer Juventino Yam Ramirez <elmer.yam@puntosingular.com>
2021-07-25 19:14:36 -0500

    agregar funcionalidad editar y buscar facturas

--- Models/DetalleFacturaModel.cs
+++ Models/DetalleFacturaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace Models
 {
     public class DetalleFacturaModel
     {
+        [Required]
         public int Id { get; set; }
         public string FacturaId { get; set; }
+        [Required]
         public ArticuloModel Articulo { get; set; }
+        [Required]
         public int CantidadArticulos { get; set; }
         public double Subtotal => CantidadArticulos * Articulo.PrecioUnitario;
         public double Iva => Subtotal * 0.16;
